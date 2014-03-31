e0cadb9661ef12d3c9b2b360d69deaad0f72e1d5
Chesco <kreymagno@54.red-217-124-187.customer.static.ccgg.telefonica.net>
2014-03-31 18:45:22 +0200

    Initial commit

--- Corona/main.lua
+++ Corona/main.lua
@@ -2,16 +2,16 @@ local infantium = require "plugin.infantium"
 
 timer.performWithDelay( 500, function()
 	-- Set up
-	infantium.init("api", true)
-	infantium.setDeveloperCredentials("androidSDK","585f946642aac847403bf6743f918892eac695f6")
+	infantium.init("beta", true)
+	infantium.setDeveloperCredentials("username","0000000000000000000000000000000000000000")
 	infantium.setDeviceInfo(500, 800)
-	infantium.setContentAppUUID("87675d63d9b84682bb4579a709dac7e2")
-	infantium.setSubContentUUID("029277974857406984835f7ef0c277cc")
+	infantium.setContentAppUUID("00000000000000000000000000000000")
+	infantium.setSubContentUUID("00000000000000000000000000000000")
 end )
 
 timer.performWithDelay( 2500, function()
 	-- Start the Gameplay
-	infantium.createGameplay("029277974857406984835f7ef0c277cc")
+	infantium.createGameplay("00000000000000000000000000000000")
 	
 	-- Elements examples
 	infantium.addElement("element_1", 200, 150, "")
